66971186fdee3f588f16b32b9a7ae9096e785ab1
Michael Gunderson <michael@portablesheep.com>
2026-04-16 21:47:43 -0500

    Fixes.

--- src/components/Annotations/__tests__/CheckboxAnnotation.test.tsx
+++ src/components/Annotations/__tests__/CheckboxAnnotation.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { CheckboxAnnotation } from '../CheckboxAnnotation';
 import type { CheckboxAnnotation as CheckboxAnnotationType } from '../../../types/annotations';
 
